5a0c69dadeaa886bcacb6a9d5622c8c269cb4c6c
Imranaliburfat11 <122956463+Imranaliburfat11@users.noreply.github.com>
2023-07-10 18:20:07 +0500

    Imranali.py
    https://github.com/Imranaliburfat11/Chaking.py.git
    CD imran
    Imranali.py

---
+++ Imranali.py
@@ -0,0 +1,34 @@
+def main_apv():
+    os.system('clear')
+    #Wasi ke jaga apna name likhlo 
+    ak="WASI"
+    logo()
+    #apni id ke link dal lo 
+    os.system('xdg-open https://www.Facebook.com/IMRANALEEi-xd')
+    try:
+    	#IMRANALEE ke jaga apna mame lagau
+        key1=open('/data/data/com.termux/files/usr/bin/.IMRANALEE-cov', 'r').read()
+    except IOError:
+        os.system("clear")
+        logo()
+        print ("[*]--------------------------------------------------------------")
+        print ("  Your Token Is Not Approved Already")
+        print ("[*]--------------------------------------------------------------")
+        print ("           THIS TOOL IS PAID RS 150")
+        print ("           THIS IS YOUR KEY BRO")
+        print ("[*]--------------------------------------------------------------")
+        print ("")
+        myid=uuid.uuid4().hex[:10].upper()
+        print ("          YOUR KEY : "+ak+myid)
+        print ("[*]--------------------------------------------------------------")
+        #IMRANALEE ke jaga apna name or kch ni cherna
+        kok=open('/data/data/com.termux/files/usr/bin/.IMRANi-cov', 'w')
+        kok.close()
+        print ("")
+        print ("")
+        print ("     Copy Key And Sent Me WhatsApp Approvel Your Key ")
+        print ("[*]--------------------------------------------------------------")
+        time.sleep(6)
+        #nichy number ki hata k apna numbr dal lo 
+        os.system("xdg-open https://wa.me/+3317792931")
+        #nichy  link hata k apni gi
